44c5eaa0019e2357a42c1a6811a86668dd88d563
Arcuman <anton.arcuman.borisov@gmail.com>
2019-04-27 16:23:20 +0300

    Initial commit

---
+++ Assets/Script/Monster.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : Unit
+{
+    protected virtual void Awake() { }
+    protected virtual void Start() { }
+    protected virtual void Update() { }
+    protected virtual void OnTriggerEnter2D(Collider2D collider)
+    {
+        Bullet bullet = collider.GetComponent<Bullet>();
+
+        if (bullet)
+        {
+            ReceiveDamage();
+        }
+
+        Character Character = collider.GetComponent<Character>();
+
+        if (Character)
+        {
+            Character.ReceiveDamage();
+        }
+
+    }
+
+
+}
+
---
+++ Assets/Script/Unit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public virtual void ReceiveDamage()
+    {
+        Die();    // чувак умирает
+    }
+
+    protected virtual void Die()
+    {
+
+        Destroy(gameObject);
+
+    }
+}
